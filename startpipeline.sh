a885fd494a99d005ab303009972ec9e9e531457c
deepakrishnapro <73492860+deepakrishnapro@users.noreply.github.com>
2020-12-12 15:08:44 -0700

    Update startpipeline.sh

--- startpipeline.sh
+++ startpipeline.sh
@@ -2,4 +2,19 @@
 
 #echo "My Build url is ----- $BUILD_URL"
 
-echo "START DATETIME is $(date)"
+echo "START DATETIME is $(date +%s)"
+
+template2='{
+    "productname" : "oscar",
+    "productversion" : "1.1",
+    "projectname":"'$TRAVIS_JOB_NAME'",
+    "environment":"Dev",
+    "projectowner":"Deepa Krishna",
+    "repositoryurl":"https://github.com/deepakrishnapro/django-oscar",
+    "url": "'$TRAVIS_JOB_WEB_URL'",
+    "commitid": "5123",
+    "jobid": "'$TRAVIS_JOB_NUMBER'",
+    "starttime": '$start_time'
+}'
+
+echo "$template2"
