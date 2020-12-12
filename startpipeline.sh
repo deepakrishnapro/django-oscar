ffa49bea5a91d97155a09b1d8ff112c76a81013d
deepakrishnapro <73492860+deepakrishnapro@users.noreply.github.com>
2020-12-12 15:34:07 -0700

    Update startpipeline.sh

--- startpipeline.sh
+++ startpipeline.sh
@@ -7,7 +7,7 @@ echo "START DATETIME is $(date +%s)"
 start_time=$(date +%s)
 
 template2='{
-    "productname" : "oscar",
+    "productname" : "oscar-from-travis",
     "productversion" : "1.1",
     "projectname":"'$TRAVIS_JOB_NAME'",
     "environment":"Dev",
@@ -20,3 +20,24 @@ template2='{
 }'
 
 echo "$template2"
+
+http_response=$(curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-w "%{http_code}" \
+-X POST --data "$template2" --url http://ec2-54-189-234-66.us-west-2.compute.amazonaws.com:8001/pipeline/start)
+
+statuscode=$(echo "$http_response" |  grep HTTP |  awk '{print $2}')
+
+if [ $statuscode -eq 200 ]; then
+	echo "Server returned: Success"
+    http_response=(${http_response[@]}) # convert to array
+    code=${http_response[-1]} # get last element (last line)
+    export PIPELINE_ID=${code%???}
+    echo "PIPELINE_ID = ${code%???}"
+    #echo $PIPELINE_ID > /var/lib/jenkins/envVars.properties
+    echo $PIPELINE_ID
+
+else
+    echo "Server did not return HTTP 200 OK"
+fi
