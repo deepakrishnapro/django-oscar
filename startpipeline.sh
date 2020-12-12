161d0e2287d907e35b2fd63a88c8d48baf9bc30b
deepakrishnapro <73492860+deepakrishnapro@users.noreply.github.com>
2020-12-12 16:52:16 -0700

    Update startpipeline.sh

--- startpipeline.sh
+++ startpipeline.sh
@@ -33,12 +33,12 @@ statuscode=$(echo "$http_response" |  grep HTTP |  awk '{print $2}')
 
 if [ "$statuscode" -eq 200 ]; then
     echo "Server returned: Success"
-    http_response=("${http_response[@]}") # convert to array
-    code=${http_response[-1]} # get last element (last line)
-    export PIPELINE_ID=${code%???}
-    echo "PIPELINE_ID = ${code%???}"
+    #http_response=("${http_response[@]}") # convert to array
+    #code=${http_response[-1]} # get last element (last line)
+    #export PIPELINE_ID=${code%???}
+    #echo "PIPELINE_ID = ${code%???}"
     #echo $PIPELINE_ID > /var/lib/jenkins/envVars.properties
-    echo "$PIPELINE_ID"
+    #echo "$PIPELINE_ID"
 else
     echo "Server did not return HTTP 200 OK"
 fi
