3adc07e3a3419856cc2b8116c6fa8aa6b7c07e8e
deepakrishnapro <73492860+deepakrishnapro@users.noreply.github.com>
2020-12-12 17:34:54 -0700

    Update upload_results.py

--- upload_results.py
+++ upload_results.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import xml.etree.ElementTree as ET
+
 def main():
     try:
         print('Calling a function upload tests ')
+        tree = ET.parse('/home/travis/build/deepakrishnapro/django-oscar/unit_report.xml')
+        print(tree.getroot())
+        root = tree.getroot()
+        print("tag=%s, attrib=%s" % (root.tag, root.attrib))
 
     except Exception as exception:
         print("Exception occurred during running upload results  for pipeline-id - Exception details : {}".format(str(exception)))
